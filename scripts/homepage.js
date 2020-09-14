bc3588a4fdb637eb2d3243cdda56b6b4a809f4e9
Eric Dudiak <admin@dudiak.com>
2020-09-13 18:40:54 -0700

    Standardize spacing.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -4,19 +4,19 @@ var neededCSSAttributes=["position", "textShadow", "background-size"];
 
 function verifyResumeBrowserSupport()
 {
-	var supported = true;
-	
-	for (index in neededCSSAttributes) {
-		supported = supported && isPropertySupported(neededCSSAttributes[index]);
-	}
-	if(supported) {
-		
-	} else {
-		window.location = "EricDudiak_Resume.pdf";
-	}
+    var supported = true;
+    
+    for (index in neededCSSAttributes) {
+        supported = supported && isPropertySupported(neededCSSAttributes[index]);
+    }
+    if(supported) {
+        
+    } else {
+        window.location = "EricDudiak_Resume.pdf";
+    }
 }
 
 function isPropertySupported(property)
 {
-	return property in document.body.style;
+    return property in document.body.style;
 }
