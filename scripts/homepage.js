83f481171a99daedf1d470dbee4edd58266184d2
Eric Dudiak <admin@dudiak.com>
2020-09-13 18:38:33 -0700

    Some cleanup of scripts and styles.

--- scripts/homepage.js
+++ scripts/homepage.js
@@ -1,6 +1,6 @@
 /* Copyright 2013 Eric Dudiak */
 
-var neededCSSAttributes=["position", "textShadow"];
+var neededCSSAttributes=["position", "textShadow", "background-size"];
 
 function verifyResumeBrowserSupport()
 {
@@ -14,28 +14,6 @@ function verifyResumeBrowserSupport()
 	} else {
 		window.location = "EricDudiak_Resume.pdf";
 	}
-}
-
-function verifyBrowserSupport()
-{
-	var supported = true;
-	
-	for (index in neededCSSAttributes) {
-		supported = supported && isPropertySupported(neededCSSAttributes[index]);
-	}
-/* 	if(supported) { */
-		$fx('#main_sidebar').style.visibility = 'visible';
-		
-		var bounceBack = function(){
-			$fx('#main_sidebar').fxAdd({type: 'marginTop', from: 90, to: 128, step: 1, delay: -1}).fxRun(null,1,null);
-		}
-		
-		$fx('#main_sidebar').fxAdd({type: 'marginTop', from: window.innerHeight, to: 90, step: -8, delay: -1}).fxRun(bounceBack,1);
-/*
-	} else {
-		window.location = "unsupported.php";
-	}
-*/
 }
 
 function isPropertySupported(property)
